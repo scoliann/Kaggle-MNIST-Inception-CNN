d1de0dd61e8ec54f92ea6c16cd7e6c86e9199c13
unknown <jesuyed@yahoo.com>
2018-03-03 18:22:38 -0500

    I can read image and store it in dir!

--- wrToImage.py
+++ wrToImage.py
@@ -1,58 +1,46 @@
 from tkinter import *
+from PIL import Image, ImageDraw
+import os
 
 canvas_width = 500
 canvas_height = 250
+white = (255, 255, 255)
+
+image1 = Image.new("RGB", (canvas_width, canvas_height), "black")
+draw = ImageDraw.Draw(image1)
+pointslist=[]
 
 def paint( event ):
-   python_green = "#476042"
+   python_green = "black"
    x1, y1 = ( event.x - 1 ), ( event.y - 1 )
    x2, y2 = ( event.x + 1 ), ( event.y + 1 )
-   w.create_oval( x1, y1, x2, y2, fill = python_green )
+   w.create_oval( x1, y1, x2, y2, fill = python_green, width=10)
+   pointslist.append((x1, y1))
 
 master = Tk()
-master.title( "Painting using Ovals" )
+master.title( "Handwriting recognition" )
 w = Canvas(master, 
            width=canvas_width, 
            height=canvas_height)
 w.pack(expand = YES, fill = BOTH)
-w.bind( "<B1-Motion>", paint )
+w.bind( "<B1-Motion>", paint)
 
-message = Label( master, text = "Press and Drag the mouse to draw" )
-message.pack( side = BOTTOM )
+
+def generateImage():
+    draw.line(pointslist, white, width=10)
+    w.postscript(file="my_drawing.ps", colormode='color')
+    filename = "my_drawing.jpg"
+    image1.save(filename)
+    exit()
     
-mainloop()
 
-'''
-from tkinter import *
-from PIL import Image
-from PIL import ImageDraw
+frame = Frame(master, bg='grey', width=20, height=40)
+frame.pack(fill='x')
+button1=Button(frame, text='Make image', command=generateImage)
+button1.pack(side='left', padx=10)
 
-width = 400
-height = 300
-center = height//2
-white = (255, 255, 255)
-green = (0,128,0)
 
-root = Tk()
-
-# Tkinter create a canvas to draw on
-cv = Canvas(root, width=width, height=height, bg='white')
-cv.pack()
-
-# PIL create an empty image and draw object to draw on
-# memory only, not visible
-image1 = Image.new("RGB", (width, height), white)
-draw = ImageDraw.Draw(image1)
-
-# do the Tkinter canvas drawings (visible)
-cv.create_line([0, center, width, center], fill='black')
-
-# do the PIL image/draw (in memory) drawings
-draw.line([0, center, width, center], green)
 
 # PIL image can be saved as .png .jpg .gif or .bmp file (among others)
-filename = "my_drawing.jpg"
-image1.save(filename)
 
-root.mainloop()
-'''
+w.mainloop()
